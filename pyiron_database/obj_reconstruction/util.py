31ec0fe975255de629f166ebf00c90bc793a3418
liamhuber <liamhuber@greyhavensolutions.com>
2026-06-26 16:14:54 -0700

    Use pyiron_snippets behind utils
    Signed-off-by: liamhuber <liamhuber@greyhavensolutions.com>

--- pyiron_database/obj_reconstruction/util.py
+++ pyiron_database/obj_reconstruction/util.py
@@ -1,42 +1,24 @@
 from typing import Any
 
+from pyiron_snippets import retrieve, versions
+
 
 def get_type(cls: Any) -> tuple[str, str, str]:
-    module = cls.__class__.__module__
-    qualname = cls.__class__.__qualname__
-    from importlib import import_module
-
-    base_module = import_module(module.split(".")[0])
-    version = (
-        base_module.__version__
-        if hasattr(base_module, "__version__")
-        else "not_defined"
-    )
-    return module, qualname, version
+    info = versions.VersionInfo.of(cls.__class__)
+    return info.module, info.qualname, info.version or "not_defined"
 
 
 def recreate_type(
     module_name: str, qualname: str, version: str, strict_version_check: bool = False
 ) -> Any:
-    from importlib import import_module
-
-    base_module = import_module(module_name.split(".")[0])
     if strict_version_check:
-        actual_version = (
-            base_module.__version__
-            if hasattr(base_module, "__version__")
-            else "not_defined"
-        )
+        actual_version = versions.get_version(module_name)
         if actual_version != version:
             raise ValueError(f"Version mismatch: {version} != {actual_version}")
-    module = import_module(module_name)
-    recreated_type = getattr(module, qualname)
-    return recreated_type
+    return retrieve.import_from_string(f"{module_name}.{qualname}")
 
 
 def recreate_obj(
     module: str, qualname: str, version: str, init_args: dict[str, Any]
 ) -> Any:
-    recreated_type = recreate_type(module, qualname, version)
-    obj = recreated_type(**init_args)
-    return obj
+    return retrieve.import_from_string(f"{module}.{qualname}")(**init_args)
